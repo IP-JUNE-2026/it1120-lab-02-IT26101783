4a5f5a2ca6cdfd0dba45358c200d1340acd03ccf
IT26101783 <it26101783@my.sliit.lk>
2026-06-17 21:51:03 +0530

    Add Q1 answer

---
+++ Q1/IT26101783Lab2Q1.java
@@ -0,0 +1,10 @@
+public class IT26101783Lab2Q1 {
+	public static void main(String args []){
+		double perimeter = 100;		
+		double length = (perimeter/2)*4/7;
+		double width = length*(4.0/3.0);
+
+		System.out.println("Length is "+ length);
+		System.out.println("Width is "+ width);
+	}
+}
