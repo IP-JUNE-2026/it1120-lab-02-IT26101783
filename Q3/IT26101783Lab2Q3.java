9c32e898d3d12c85c3a918c76dca670f7bcc218e
IT26101783 <it26101783@my.sliit.lk>
2026-06-17 21:52:17 +0530

    Add Q3 answer

---
+++ Q3/IT26101783Lab2Q3.java
@@ -0,0 +1,9 @@
+public class IT26101783Lab2Q3 {
+	public static void main(String args []){
+		int SideA=3;
+		int SideB=4;
+		double Hypotenuse=Math.sqrt(SideA*SideA+SideB*SideB);
+
+		System.out.println("Hypotenuse is "+Hypotenuse);	
+	}
+}
