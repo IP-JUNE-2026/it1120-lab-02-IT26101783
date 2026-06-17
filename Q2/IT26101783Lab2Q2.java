ca16980a1cce2665804cf478be5e89affe0367f0
IT26101783 <it26101783@my.sliit.lk>
2026-06-17 21:51:32 +0530

    Add Q2 answer

---
+++ Q2/IT26101783Lab2Q2.java
@@ -0,0 +1,12 @@
+public class IT26101783Lab2Q2 {
+	public static void main(String args []){
+		double sidelength = 10;
+		double pi = 3.14;
+
+		double ropelength = 4*sidelength;
+		
+		double radius=ropelength/(2*pi);
+
+		System.out.println("Radius of the circular fence is "+radius);
+	}
+}
